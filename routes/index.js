4906959ede803eead99b39305c5b1e7675d685fc
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-02-07 20:43:27 +0100

    Acción de juego.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const tablero = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
 /* Pantalla principal del juego. */
 router.get('/', function(req, res, next) {
-  const tablero = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 1, 2]
-  ];
+  res.render('index', { title: 'Tres en raya', tablero });
+});
+
+router.post('/ponerficha', function(req, res, next) {
+  const {fila, columna} = req.body;
+  tablero[fila][columna] = 1;
   res.render('index', { title: 'Tres en raya', tablero });
 });
 
