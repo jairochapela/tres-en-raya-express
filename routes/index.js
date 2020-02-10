8bab73d1ba21f12c2b7ec3bf6677dfe056f5a7c7
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-02-10 17:42:17 +0100

    Cambios de turno.

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,45 @@ const tablero = [
 ];
 
 var jugadores = 0;
+var turno = 1;
 
 /* Pantalla principal del juego. */
 router.get('/', function(req, res, next) {
   const session = req.session;
   if (!session.jugador) {
     jugadores++;
+    if (jugadores > 2) {
+      res.status(403);
+    }
     session.jugador = jugadores;
   }
-  res.render('index', { title: 'Tres en raya', tablero });
+  const meToca = (turno == session.jugador);
+  res.render('index', { title: 'Tres en raya', tablero, meToca });
 });
 
 router.post('/ponerficha', function(req, res, next) {
   const {fila, columna} = req.body;
   const jugador = req.session.jugador;
   tablero[fila][columna] = jugador;
-  res.render('index', { title: 'Tres en raya', tablero });
+
+  // Alternamos el turno
+  if (turno == 1) {
+    turno = 2;
+  } else {
+    turno = 1;
+  }
+
+  res.redirect("/");
 });
+
+
+router.get("/miturno", function(req, res, next) {
+  const meToca = (turno == req.session.jugador);
+  if (meToca) {
+    res.status(200).json(true); //ok
+  } else {
+    res.status(400).json(false); //fallo (si no me toca)
+  }
+})
 
 module.exports = router;
